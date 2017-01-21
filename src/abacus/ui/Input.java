aa86c5d250c4d266c0d7b0a4db2aa02268ff931b
AntiPyro <acfelder@mtu.edu>
2017-01-21 16:25:19 -0500

    Commented /src/abacus/ui/ files

--- src/abacus/ui/Input.java
+++ src/abacus/ui/Input.java
@@ -1,3 +1,6 @@
+/* Input.java
+ * Class that handles all of the input for the game
+ */
 package abacus.ui;
 
 import java.util.HashMap;
@@ -5,26 +8,33 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/* Input class for handling input for the game
+ */
 public class Input {
 
+	/* Instance Variables */
     private Set<Integer> justDown;
     private Set<Integer> downKeys;
     private Map<String, Integer> defKeys;
     
+	// Constrctor for the class
     public Input() {
         downKeys = new HashSet<>();
         justDown = new HashSet<>();
         defKeys = new HashMap<>();
     }
     
+	// Removes all keys from the just down set
     public void update() {
         justDown.clear();
     }
     
+	// Registers a key for input
     public void registerKey(int key, String name) {
         defKeys.put(name, key);
     }
     
+	// Sets a key in the position it is
     public void setKey(int key, boolean down) {
         if (down) {
             if (!getKey(key)) {
@@ -37,14 +47,17 @@ public class Input {
         }
     }
     
+	// Returns the most recently pressed down key
     public boolean getJustDownKey(int key) {
         return justDown.contains(key);
     }
     
+	// Returns the most recent held down key
     public boolean getKey(int key) {
         return downKeys.contains(key);
     }
     
+	// Returns the value of the given key
     public boolean getKey(String key) {
         Integer val = defKeys.get(key);
         if (val == null) {
@@ -55,6 +68,7 @@ public class Input {
         }
     }
     
+	// Returns the value of the given recently pressed down key
     public boolean getJustDownKey(String key) {
         Integer val = defKeys.get(key);
         if (val == null) {
@@ -65,8 +79,8 @@ public class Input {
         }
     }
     
+	// Returns true if any key is down. False otherwise
     public boolean anyKeyJustDown() {
         return !justDown.isEmpty();
     }
-    
 }
--- src/abacus/ui/Window.java
+++ src/abacus/ui/Window.java
@@ -1,18 +1,30 @@
+/* Window.java
+ * Handles the window of the game
+ */
+
 package abacus.ui;
 
+/* Window interface to handle the window of the game 
+ */
 public interface Window {
+	
+	/* Function Descriptions */
 
+	// Title get and set
     void setTitle(String title);
     String getTitle();
     
+	// Resolution functions
     void setResolution(int width, int height);
     void setFullscreen(boolean fs);
     boolean isFullscreen();
     int getWidth();
     int getHeight();
     
+	// Virtual resolution function
     void setVirtualResolution(int width, int height);
     
+	// Visiblity functions
     void show();
     void hide();
     boolean isVisible();
