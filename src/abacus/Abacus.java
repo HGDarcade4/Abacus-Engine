c4dde09b25f7ea1ea8eb9813d8908b03e4f6907a
AntiPyro <acfelder@mtu.edu>
2017-01-21 15:52:05 -0500

    Commented most of the files in the /src/abacus directory

--- src/abacus/Abacus.java
+++ src/abacus/Abacus.java
@@ -1,8 +1,13 @@
+/* Abacus.java
+ * Handles global game information
+ */
+
 package abacus;
 
+/* Abacus class to hold the global game information
+ */
 public class Abacus {
 
     public static final String VERSION_MAJOR = "0";
     public static final String VERSION_MINOR = "4";
-    
 }
--- src/abacus/GameStateManager.java
+++ src/abacus/GameStateManager.java
@@ -1,24 +1,33 @@
+/* GameStateManager.java
+ * This class manages the current state of the game
+ */
+
 package abacus;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
+/* GameStateManager class for handling the current state of the game
+ */
 public class GameStateManager {
 
+	/* Instance Variables */
     private Map<Integer, GameState> stateMap;
-    
     private Stack<GameState> active;
     
+	// Constructor
     public GameStateManager() {
         stateMap = new HashMap<>();
         active = new Stack<>();
     }
     
+	// Puts the given state into the state map
     public void registerState(int key, GameState state) {
         stateMap.put(key, state);
     }
     
+	// Pushes the given state to the active stack
     public void pushState(int key) {
         GameState state = stateMap.get(key);
         
@@ -31,6 +40,7 @@ public class GameStateManager {
         enter();
     }
     
+	// Removes the current state from the active stack
     public void popState() {
         if (!noActiveStates()) {
             exit();
@@ -39,6 +49,7 @@ public class GameStateManager {
         }
     }
     
+	// Swap the currently running state with the given one
     public void swapState(int key) {
         GameState state = stateMap.get(key);
         
@@ -58,38 +69,44 @@ public class GameStateManager {
         }
     }
     
+	// Returns true if there is not an active state. False otherwise
     public boolean noActiveStates() {
         return active.isEmpty();
     }
     
+	// Returns the currently active state
     public GameState getCurrentState() {
         return active.peek();
     }
     
+	// Returns all of the states on the active stack
     public GameState[] getActiveStates() {
         return active.toArray(new GameState[active.size()]);
     }
     
+	// Returns all of the registered states in the game
     public GameState[] getRegisteredStates() {
         return stateMap.values().toArray(new GameState[stateMap.values().size()]);
     }
     
+	// Exits the currently running state
     private void exit() {
         if (!noActiveStates()) {
             getCurrentState().exit();
         }
     }
     
+	// Enters the currently running state
     private void enter() {
         if (!noActiveStates()) {
             getCurrentState().enter();
         }
     }
     
+	// Pauses the currently running state
     private void pause() {
         if (!noActiveStates()) {
             getCurrentState().pause();
         }
     }
-    
 }
