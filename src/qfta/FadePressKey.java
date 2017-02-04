c4855d045b45a51fcc78d3255729b20195797447
Adrian Felder <acfelder@mtu.edu>
2017-02-04 15:00:04 -0500

    Accidently worked in master. Added a FadePressKey class to handle scenes that need a 'Press Any Key...' type message to appear. Committing these changes before working on existing code and then switching to a new branch

---
+++ src/qfta/FadePressKey.java
@@ -0,0 +1,21 @@
+package qfta;
+
+import abacus.graphics.GameFont;
+import abacus.graphics.Renderer;
+
+public class FadePressKey {
+
+	private String message;
+	private FadeTimer fade;
+	
+	public FadePressKey(String message) {
+		this.message = message;
+		this.fade = new FadeTimer(10, 30, 10, 30, 0);
+	}
+	
+	public void render(Renderer renderer, GameFont font) {
+        int x = (int) (renderer.getWidth()/2 - font.getWidth(this.message)/2);
+        font.setAlpha(this.fade.getAlpha());
+        font.draw(this.message, x, 24);
+	}
+}
